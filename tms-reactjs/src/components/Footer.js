aefd77b0dd7442dd51a1b2dfc8b9e18e6f66abf2
Muneel Haider <muneelhaider@gmail.com>
2024-07-29 23:08:03 +0500

    Updated Footer.

--- tms-reactjs/src/components/Footer.js
+++ tms-reactjs/src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Made by Muneel Haider</p>
+      <p>Made by <a href="https://www.linkedin.com/in/muneel-haider/" target="_blank" rel="noopener noreferrer">Muneel Haider</a></p>
       <p>Tech Stack: React, ASP.NET Core, SQL Server</p>
+      <div className="footer-icons">
+        <a href="https://www.linkedin.com/in/muneel-haider/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="icon" />
+        </a>
+        <a href="https://github.com/MuneelHaider" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="icon" />
+        </a>
+      </div>
     </footer>
   );
 };
