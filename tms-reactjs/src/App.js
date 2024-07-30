55ab1cf178892e8fa251d788e0fd9b3f41581fa7
Muneel Haider <muneelhaider@gmail.com>
2024-07-30 16:55:22 +0500

    Finishing touches.

--- tms-reactjs/src/App.js
+++ tms-reactjs/src/App.js
@@ -14,6 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  
   const navigate = useNavigate();
   const [role, setRole] = useState(localStorage.getItem('role'));
 
